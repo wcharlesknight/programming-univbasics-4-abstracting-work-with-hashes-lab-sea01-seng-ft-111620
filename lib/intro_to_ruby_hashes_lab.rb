7957400163a5d10008b10308735e3ee131e146d9
Charlie Knight <williamchknight@gmail.com>
2020-11-05 23:50:30 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -3,7 +3,8 @@ def my_hash_creator(key, value)
 end
 
 def read_from_hash(hash, key)
-  # return the correct value using the hash and key parameters
+ hash = {key: value}
+ value
 end
 
 def update_counting_hash(hash, key)
